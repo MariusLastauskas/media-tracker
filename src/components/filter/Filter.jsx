e15488cc4e56662ff316a3ad692d7c79ff0c1468
MariusLastauskas <marius20122@gmail.com>
2020-04-04 17:29:14 +0300

    basic layout added

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './filter.scss';
+
+const Filter = () => (
+    <div className="filter">
+        <div className="filter__holder">filter here</div>
+    </div>
+);
+
+export default Filter;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './header.scss';
+
+const Header = () => (
+    <header className="header">
+        <div className="header__holder">header here</div>
+    </header>
+);
+
+export default Header;
