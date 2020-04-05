18a40c8f4ed541d99f3d06f161f454a53a932c78
MariusLastauskas <marius20122@gmail.com>
2020-04-05 15:59:47 +0300

    header navigation added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { Navigation } from '../navigation';
+
 import './header.scss';
 
 const Header = () => (
     <header className="header">
-        <div className="header__holder">header here</div>
+        <div className="header__holder">
+            <Navigation />
+            <div className="column">profile</div>
+        </div>
     </header>
 );
 
